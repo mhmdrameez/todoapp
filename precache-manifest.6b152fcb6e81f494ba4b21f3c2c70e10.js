fb34a8535f19ac41c467261342ff9dd04ce7cf57
mhmdrameez <muhammedrameez2000@gmail.com>
2020-06-07 15:15:33 +0530

    Updates

---
+++ precache-manifest.6b152fcb6e81f494ba4b21f3c2c70e10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e12d289b3b0b0d0f68c1ac66e4fd3a",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "5d9fdcbde7c1f261181a",
+    "url": "/todoapp/static/css/main.18e41fe2.chunk.css"
+  },
+  {
+    "revision": "ebcc2a0de3a6220c4bc4",
+    "url": "/todoapp/static/js/2.9f153dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.9f153dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d9fdcbde7c1f261181a",
+    "url": "/todoapp/static/js/main.4e0d947e.chunk.js"
+  },
+  {
+    "revision": "9b2f915f09a204b07370",
+    "url": "/todoapp/static/js/runtime-main.d779cf41.js"
+  }
+]);
